abafbfb8f0945476672552d8a814417592790cd1
DawidKumor <dawidkumor@gmail.com>
2026-06-11 02:42:24 +0200

    prettier update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ console.log(`Name: ${name}, Age: ${age}`);
 const header = document.querySelector(`h1`);
 console.log(header);
 header.innerHTML = "Jebać pis i konederacje";
-console.log(header.style)
+console.log(header.style);
 header.style.color = "pink";
 header.style.fontSize = "3rem";
 header.style.textTransform = "uppercase";
@@ -13,24 +13,27 @@ header.style.textAlign = "center";
 header.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)";
 const header2 = document.querySelector(`.page-header__subheading--js`);
 if (header2) {
-header2.style.color = "blue";
-header2.style.fontSize = "2rem";
-header2.style.textAlign = "center";
-header2.style.textShadow = "1px 1px 3px rgba(0, 0, 0, 0.3)";    
-header2.innerHTML = "Inżynier automatyki i robotyki z akademii tańca i chumoru";
+  header2.style.color = "blue";
+  header2.style.fontSize = "2rem";
+  header2.style.textAlign = "center";
+  header2.style.textShadow = "1px 1px 3px rgba(0, 0, 0, 0.3)";
+  header2.innerHTML =
+    "Inżynier automatyki i robotyki z akademii tańca i chumoru";
 }
 const deathStar = {
-    diameter: 120000,
-    fire: (target) => { console.log(`${target} destroyed!`) },
-    isOperating: true,
-    levels: 357,
-    name: "Death Star",
-    population: 1000000,
-    commander: {
-        name: "Darth Vader",
-        rank: "Sith Lord"
-    }
-}
+  diameter: 120000,
+  fire: (target) => {
+    console.log(`${target} destroyed!`);
+  },
+  isOperating: true,
+  levels: 357,
+  name: "Death Star",
+  population: 1000000,
+  commander: {
+    name: "Darth Vader",
+    rank: "Sith Lord",
+  },
+};
 console.log(deathStar.name);
 deathStar.fire("Alderaan");
 const fName = "Edward";
@@ -39,23 +42,22 @@ const fullName = `${fName} ${lName}`;
 console.log(`Hello, this is ${fullName} in the ship!`);
 
 const greet = (name, age) => {
-    return `Hello, my name is ${name} and I am ${age} years old.`;
-}
+  return `Hello, my name is ${name} and I am ${age} years old.`;
+};
 console.log(greet("Dawid", 32));
 
-const button = document.querySelector('.page-header__button--js')
+const button = document.querySelector(".page-header__button--js");
 if (button) {
-button.classList.add('testowa-klasa');
-console.log(button.classList);
-const clickFunction = (e) => {
-    const header = document.querySelector('.page-header__heading--js');
+  button.classList.add("testowa-klasa");
+  console.log(button.classList);
+  const clickFunction = (e) => {
+    const header = document.querySelector(".page-header__heading--js");
     header.innerHTML = "click click clack symphony";
+  };
+  button.addEventListener("click", clickFunction);
 }
-button.addEventListener('click', clickFunction)
-}
-const hamburger = document.querySelector('.hamburger--js');
-hamburger.addEventListener('click', () => {
-const navigation = document.querySelector('.navigation--js');
-navigation.classList.toggle('navigation--open');
-})
-
+const hamburger = document.querySelector(".hamburger--js");
+hamburger.addEventListener("click", () => {
+  const navigation = document.querySelector(".navigation--js");
+  navigation.classList.toggle("navigation--open");
+});
